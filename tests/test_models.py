f2e547fa58ee92e5e2ff9c274abcb0629eb526f9
iiogmgo <iiogmgo@gmail.com>
2019-07-26 18:22:28 +0900

    Add testcase : Url model's validation logic test

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,3 +15,14 @@ def test_fail_to_insert_unique_name(url, session):
         assert False
     except Exception:
         assert True
+
+
+def test_validation_logic(session):
+    new_url = Url(name='이것은 테스트! HI', destination='www.naver.com')
+    session.add(new_url)
+    session.commit()
+    try:
+        url = Url.query.filter_by(name='이것은테스트HI').first()
+        assert url.destination == 'http://www.naver.com'
+    except Exception:
+        assert False
