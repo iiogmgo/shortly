b794a4cc8f392d66e898e2892af76ff08f078064
iiogmgo <iiogmgo@gmail.com>
2019-07-26 22:08:26 +0900

    Implement error handling when the name of already existing url is inserted

--- shortly/routes.py
+++ shortly/routes.py
@@ -1,12 +1,20 @@
 from flask import Blueprint, request, redirect, url_for, render_template
 from shortly.models import Url
 from shortly import db
+from sqlalchemy import exc
+from shortly.utils import get_random_shortly_name
 
 bp = Blueprint('routes', __name__)
 
 
 @bp.route('/')
 def index():
+    if 'name' in request.args:
+        name = request.args['name']
+        destination = request.args['destination']
+        message = request.args['message']
+        return render_template('index.html', name=name, destination=destination, message=message)
+
     return render_template('index.html')
 
 
@@ -15,19 +23,28 @@ def urls():
     return render_template('urls.html', urls=Url.query.all())
 
 
-@bp.route('/urls', methods=['POST'])
-def create():
-    from shortly.utils import get_random_shortly_name
-
-    destination = request.form['destination']
-    name = request.form['name'] if request.form['name'] else get_random_shortly_name()
-
-    # todo : 자동 생성된 hash로 인해 name unique 에러 뜰 경우 처리
+def insert_url_with_retry(is_hash_name, name, destination):
     url = Url(name=name, destination=destination)
     db.session.add(url)
-    db.session.commit()
+    try:
+        db.session.commit()
+        return redirect(url_for('routes.urls'))
+    except exc.IntegrityError:
+        if is_hash_name:
+            insert_url_with_retry(is_hash_name, get_random_shortly_name(), destination)
+        else:
+            return redirect(url_for('index',
+                                    name=name, destination=destination,
+                                    message='The name({}) already registered.'.format(name)))
 
-    return redirect(url_for('routes.urls'))
+
+@bp.route('/urls', methods=['POST'])
+def create():
+    is_hash_name = False if request.form['name'] else True
+    destination = request.form['destination']
+    name = get_random_shortly_name() if is_hash_name else request.form['name']
+
+    return insert_url_with_retry(is_hash_name, name, destination)
 
 
 @bp.route('/urls/delete', methods=['POST'])
